f337c7a935bd9ae8508fb07fa1774406aa59506e
Rabishankar1 <rabishankar.panigrahi14@gmail.com>
2025-02-14 15:38:29 +0530

    removing-slash

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173","http://localhost:5174", "https://saas-frontend-sigma.vercel.app/"],
+    origin: ["http://localhost:5173","http://localhost:5174", "https://saas-frontend-sigma.vercel.app"],
     credentials: true, 
   })
 );
