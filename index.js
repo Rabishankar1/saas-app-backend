3c4aa6a57ade174a99905a45a5f110e6bfa79199
Rabishankar1 <rabishankar.panigrahi14@gmail.com>
2025-02-14 15:33:56 +0530

    all-origin-removed

--- index.js
+++ index.js
@@ -28,11 +28,8 @@ app.use(express.json());
 
 app.use(
   cors({
-    // origin: ["http://localhost:5173","http://localhost:5174", "https://saas-frontend-sigma.vercel.app/"],
-    origin: "*",
+    origin: ["http://localhost:5173","http://localhost:5174", "https://saas-frontend-sigma.vercel.app/"],
     credentials: true, 
-    methods: ["GET", "POST", "PUT", "DELETE"], 
-    allowedHeaders: ["Content-Type", "Authorization"], 
   })
 );
 
