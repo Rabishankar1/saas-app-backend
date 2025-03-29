c454f2e2cd28ac1ad7faae6b410574bd8fed6067
Rabishankar1 <rabishankar.panigrahi14@gmail.com>
2025-03-29 19:13:57 +0530

    formatting

--- Middlewares/AuthMiddleware.js
+++ Middlewares/AuthMiddleware.js
@@ -3,25 +3,25 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
 module.exports.userVerification = (req, res, next) => {
-  const token = req.cookies.token;
+  const token = req?.cookies?.token;
   if (!token) {
     return res.status(401).json({ status: false });
   }
   jwt.verify(token, process.env.TOKEN_KEY, async (err, data) => {
     if (err) {
-      return res.status(401).json({ status: false });
+      return res
+        .status(401)
+        .json({ status: false, message: "Error in verifying token" });
     } else {
       const user = await User.findById(data.id);
-      req.user = {
-        id: data.id,
-        username: user.username,
-        email: user.email,
-        createdAt: user.createdAt,
-        subscriptionPlan: user.subscriptionPlan,
-      };
       if (user) {
-        req.user = user;
-        // res.json({ status: true, user: user.username });
+        req.user = {
+          id: data.id,
+          username: user.username,
+          email: user.email,
+          createdAt: user.createdAt,
+          subscriptionPlan: user.subscriptionPlan,
+        };
         next();
       } else {
         return res.status(401).json({ status: false });
